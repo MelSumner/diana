82484de2c955820f4e64e0c8b0e52c7d44db8b82
Melanie Sumner <melaniersumner@gmail.com>
2019-07-31 12:05:24 -0500

    organized test

--- test/tests.js
+++ test/tests.js
@@ -1,36 +1,43 @@
+import { test } from 'qunit';
+
 const { spawnChrome } = require("chrome-debugging-client");
 
-QUnit.test( "hello test", function( assert ) {
+test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
 
-QUnit.test('a11y node count', async function ( assert ) {
+test('a11y node count', async function ( assert ) {
   debugger;
-// spawn Chrome
-// tell it what to do if it errors
-// create/set the target (URL)
-// send the target to the browser
-// attach the target
-// enable the accessibility tree API
-// get the full AX tree (getFullAXTree)
-// test/count how many data nodes there are
-// (eventually:?) de-spawn chrome
-    const chrome = spawnChrome();
-    const browser = chrome.connection;
-    browser.on("error", err => {
-      // underlying connection error or error dispatching events.
-      console.error(`connection error ${err.stack}`);
-    });
-    const { targetId } = await browser.send('Target.createTarget', {
-      url: 'https://diana-app.netlify.com',
-    });
-    await browser.send('Target.activateTarget', { targetId });
-    const page = await browser.attachToTarget(targetId);
-    
-    await page.send('Accessibility.enable');
+  // spawn Chrome
+  const chrome = spawnChrome();
+  const browser = chrome.connection;
 
-    let fullAXTree = await page.send('Accessibility.getFullAXTree');
+  // handle errors
+  browser.on("error", err => {
+    // underlying connection error or error dispatching events.
+    console.error(`connection error ${err.stack}`);
+  });
 
-    assert.equal(fullAXTree.nodes.length, 14); 
-    await browser.send("Target.closeTarget", { targetId });
+  // create/set the target (URL)
+  const { targetId } = await browser.send('Target.createTarget', {
+    url: 'https://diana-app.netlify.com',
+  });
+
+  // send the target to the browser
+  await browser.send('Target.activateTarget', { targetId });
+
+  // attach the target
+  const page = await browser.attachToTarget(targetId);
+  
+  // enable the accessibility tree API
+  await page.send('Accessibility.enable');
+
+  // get the full AX tree (getFullAXTree) of the page
+  let fullAXTree = await page.send('Accessibility.getFullAXTree');
+
+  // test/count how many data nodes there are
+  assert.equal(fullAXTree.nodes.length, 14); 
+
+  // de-spawn chrome
+  await browser.send("Target.closeTarget", { targetId });
 });
