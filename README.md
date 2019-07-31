dd05cc828d05bac36e65367a00bfb408a46cb2cd
Melanie Sumner <melaniersumner@gmail.com>
2019-07-31 11:54:55 -0500

    added test folder and qunit test structure

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 A tool for hunting accessibility nodes (named after Diana, the goddess of the hunt). 
 
-References needed:
+## References
 - [Qunit](https://qunitjs.com/)
 - [Chrome DevTools Protocol Viewer](https://chromedevtools.github.io/devtools-protocol/tot/Accessibility)
 - [Chrome Debugger Client](https://github.com/TracerBench/chrome-debugging-client)
 - [Sample app used in this repo](https://diana-app.netlify.com/) - it's specifically semantic HTML, and is anticipated to grow in complexity as this tool progresses. 
+
+## Notes
+
+- Chrome vs headless Chrome seems to return a different count for `Accessibility.getFullAxTree` 
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,36 @@
-// const { spawnChrome } = require("chrome-debugging-client");
+const { spawnChrome } = require("chrome-debugging-client");
 
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
+});
+
+QUnit.test('a11y node count', async function ( assert ) {
+  debugger;
+// spawn Chrome
+// tell it what to do if it errors
+// create/set the target (URL)
+// send the target to the browser
+// attach the target
+// enable the accessibility tree API
+// get the full AX tree (getFullAXTree)
+// test/count how many data nodes there are
+// (eventually:?) de-spawn chrome
+    const chrome = spawnChrome();
+    const browser = chrome.connection;
+    browser.on("error", err => {
+      // underlying connection error or error dispatching events.
+      console.error(`connection error ${err.stack}`);
+    });
+    const { targetId } = await browser.send('Target.createTarget', {
+      url: 'https://diana-app.netlify.com',
+    });
+    await browser.send('Target.activateTarget', { targetId });
+    const page = await browser.attachToTarget(targetId);
+    
+    await page.send('Accessibility.enable');
+
+    let fullAXTree = await page.send('Accessibility.getFullAXTree');
+
+    assert.equal(fullAXTree.nodes.length, 14); 
+    await browser.send("Target.closeTarget", { targetId });
 });
